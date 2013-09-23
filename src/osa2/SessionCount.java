b648ca0487e2fc9c1a5bb6d168c84d34a3d9d2d6
Karl <karl.kadalipp@itcollege.ee>
2013-09-23 23:36:13 +0300

    kijklhklbj

--- src/osa2/SessionCount.java
+++ src/osa2/SessionCount.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
 
 //Teine servlet n2itab avatud sessioonide arvu.
 //Sessioonide lugemiseks kasutada listener-i. N2ide loengu
