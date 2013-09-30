858b2305d854599874f7d761bd4c4f02696cb151
Karl <karl.kadalipp@itcollege.ee>
2013-09-30 16:58:16 +0300

    upupup

--- src/osa2/HomePage.java
+++ src/osa2/HomePage.java
@@ -18,16 +18,16 @@ public class HomePage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String paramText = "param";
 		HttpSession session = request.getSession();
-		String sessionId = session.getId();
 		String param = request.getParameter(paramText);
-		String sessionParam = (String) session.getAttribute(paramText);
-		if (!param.isEmpty()) {
+		String sessionId = session.getId();
+		String sessionParam = (String)session.getAttribute(paramText);
+		if(param != null){
 			request.getSession().setAttribute(paramText, param);
 			sessionParam = param;
 		}
 		response.getWriter().println("Hello");
-		response.getWriter().println("Your session id is "+sessionId);
-		response.getWriter().println("Session attribute is "+sessionParam);	
+		response.getWriter().println("Your session id is " + sessionId);
+		response.getWriter().println("Session attribute is " + sessionParam);		
 	}
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
